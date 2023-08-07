61c849d42cbce63659337681d30d56dddeb8ab5b
Amidat <ogunbiyiamidat65@gmail.com>
2023-08-07 17:56:04 +0100

    pushing from Amidat

---
+++ Router/authRouter.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import upload from "../Config/multer";
+import { createUser, deleteOneUser, findOneUser, findUsers, signinUser, updateOneUser } from "../Controller/authController";
+
+
+const router = express.Router();
+
+router.route("/get-User").get(findUsers);
+router.route("/register-Users").post(upload, createUser);
+router.route("/register-Users").post(signinUser);
+router.route("/:UserID/get-one-User").get(findOneUser);
+router.route("/:UserID/update-one-User").patch(updateOneUser);
+router.route("/:UserID/delete-one-User").delete(deleteOneUser);
