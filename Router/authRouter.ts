d700bab6b0a06088d9b424ac82e1cc3ded019565
Amidat <ogunbiyiamidat65@gmail.com>
2023-08-08 14:01:43 +0100

    master

--- Router/authRouter.ts
+++ Router/authRouter.ts
@@ -7,7 +7,9 @@ const router = express.Router();
 
 router.route("/get-User").get(findUsers);
 router.route("/register-Users").post(upload, createUser);
-router.route("/register-Users").post(signinUser);
+router.route("/signin-Users").post(signinUser);
 router.route("/:UserID/get-one-User").get(findOneUser);
 router.route("/:UserID/update-one-User").patch(updateOneUser);
 router.route("/:UserID/delete-one-User").delete(deleteOneUser);
+
+export default router
--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
+
 import cors from "cors"
 import express, { Application } from "express"
+import task from "./Router/taskRouter"
+import auth from  "./Router/authRouter"
 
 const main =(app:Application)=>{
     app.use(express.json())
     app.use(cors())
+
+    app.use("/api/v1/task", task)
+    app.use("/api/v1/auth", auth)
 }
 
 export default main
